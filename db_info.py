b52cb874cbb8664f354feadc5de6d704221544af
warrenskim <42902241+warrenskim@users.noreply.github.com>
2021-08-18 16:30:30 -0700

    config vars on heroku

--- db_info.py
+++ db_info.py
@@ -1,4 +1,6 @@
-host = ''
-user = ''
-password = ''
-db = ''
+import os
+
+host = os.environ.get("DB_HOST")
+user = os.environ.get("DB_USER")
+password = os.environ.get("DB_PASSWORD")
+db = os.environ.get("DB")
